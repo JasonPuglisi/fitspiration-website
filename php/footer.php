4562ffeaf65e1a4e4fbd2ffd45d9b8593b5c037d
Jason Puglisi <jason@puglisi.im>
2014-12-08 18:15:34 -0500

    Updated footer style and links

--- php/footer.php
+++ php/footer.php
@@ -1,26 +1,33 @@
-		<div class='footer'>
-			<div class='container row text-center'>
-				<hr>
-				<div class='col-md-4'>
-					<p><i class='fa fa-copyright'></i> FITspiration 2014</p>
-				</div>
-				<div class='col-md-4'>
-						<p>
-							<a href='mailto:contact@findanewmax.com' class='icon-link'><i class='fa fa-at fa-2x'></i></a>
-							&nbsp;&nbsp;&nbsp;
-							<a href='https://twitter.com/FITspirationVEI' class='icon-link'><i class='fa fa-twitter fa-2x'></i></a>
-							&nbsp;&nbsp;&nbsp;
-							<a href='http://instagram.com/fitspirationvei' class='icon-link'><i class='fa fa-instagram fa-2x'></i></a>
-						</p>
-				</div>
-				<div class='col-md-4'>
-					<p><i class='fa fa-institution'></i> For educational purposes only</p>
+		<br>
+	</div>
+
+	<div class='section-gray'>
+		<div class='container'>
+			<br>
+			<div class='footer'>
+				<div class='container row text-center'>
+					<div class='col-md-4'>
+						<p><i class='fa fa-copyright'></i> FITspiration 2014</p>
+						<p><i class='fa fa-institution'></i> For educational purposes only</p>
+						<p><i class='fa fa-map-marker'></i> Made in Robbinsville, New Jersey</p>
+						<br>
+					</div>
+					<div class='col-md-4'>
+						<p><a href='mailto:contact@findanewmax.com'><i class='fa fa-envelope'></i> Email: contact@findanewmax.com</a></p>
+						<p><a href='https://twitter.com/FITspirationVEI'><i class='fa fa-twitter'></i> Twitter: @FITspirationVEI</a></p>
+						<p><a href='http://instagram.com/fitspirationvei'><i class='fa fa-instagram'></i> Instagram: FITspirationVEI</a></p>
+						<p><a href='tel:6986329859'><i class='fa fa-phone'></i> Phone: (609) 632-0950</a></p>
+						<br>
+					</div>
+					<div class='col-md-4'>
+						<p><a href='/staff'><i class='fa fa-user'></i> Learn about our staff</a></p>
+						<p><a href='https://github.com/JasonPuglisi/fitspiration-website'><i class='fa fa-github'></i> Check out our source code</a></p>
+						<p><a href='/'><i class='fa fa-home'></i> Go to our home page</a></p>
+						<br>
+					</div>
 				</div>
 			</div>
 		</div>
-
-		<div style='padding-bottom: 20px'></div>
-
 	</div>
 
 	<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>
