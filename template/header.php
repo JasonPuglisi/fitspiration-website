056aff1926390ca28331e20ead5752b1a6b5a9ae
Jason Puglisi <jason@puglisi.im>
2015-09-07 16:12:17 -0400

    Removed Google Analytics tracking

--- template/header.php
+++ template/header.php
@@ -145,17 +145,6 @@ function get_days_ago_string($date_string)
 		<script src='https://oss.maxcdn.com/respond/1.4.2/respond.min.js'></script>
 	<![endif]-->
 
-	<script>
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-		ga('create', 'UA-39410002-8', 'auto');
-		ga('require', 'displayfeatures');
-		ga('send', 'pageview');
-	</script>
-
 </head>
 <body>
 
